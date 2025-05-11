35b6cbecacc5f1bdf9e9725ccf3d407cfbe9a0fd
josh <jaaurellano@gbox.ncf.edu.ph>
2025-05-11 18:56:31 +0800

    Added logout funtion to Token controller

--- controller/tokenController.js
+++ controller/tokenController.js
@@ -1,4 +1,3 @@
-const express = require('express');
 const jwt = require('jsonwebtoken');
 
 const tokenChecker = async(req,res) => {
@@ -20,4 +19,23 @@ const tokenChecker = async(req,res) => {
         })
     }
 }
-module.exports = {tokenChecker}
+const logout = async (req,res) => {
+    const token = req.cookies.token;
+    if(!token) {
+        return res.status(401).json({
+            error:'No session found'
+        })
+    }
+    try {
+        res.clearCookie('token')
+        res.send({
+                message:'User successfully logged out'
+            })
+        } catch (error) {
+            return res.status(403).json({
+                error:error
+            })
+        }
+
+}
+module.exports = {tokenChecker,logout}
--- routes/tokenRoutes.js
+++ routes/tokenRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const {tokenChecker} = require('../controller/tokenController');
+const {tokenChecker,logout} = require('../controller/tokenController');
+const authenticateToken = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/',tokenChecker);
+router.post('/logout',authenticateToken,logout);
 
 module.exports = router;
